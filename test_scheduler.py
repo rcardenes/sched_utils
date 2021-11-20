05cf98b1e7e2c32f8a004a43319879328135fbcd
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2021-11-20 12:50:41 -0300

    Add some configuration options

--- test_scheduler.py
+++ test_scheduler.py
@@ -6,16 +6,18 @@ import logging
 import signal
 from random import randint
 
-from .scheduler_bin import SchedulerBin, Task
+from .scheduler_bin import SchedulerBin, Task, DEFAULT_TIMEOUT, DEFAULT_POOL_SIZE
+
+DEFAULT_PERIOD = 5
 
 def get_new_task():
     prio, runtime = randint(0, 10), randint(3, 15)
     return Task(prio, runtime)
 
-async def main():
+async def main(args):
     done = asyncio.Event()
 
-    the_bin = SchedulerBin()
+    the_bin = SchedulerBin(args.size, args.timeout)
 
     def shutdown():
         done.set()
@@ -28,7 +30,7 @@ async def main():
         task = get_new_task()
         logging.info(f"Scheduling a job for {task}")
         the_bin.schedule(task)
-        await asyncio.sleep(5)
+        await asyncio.sleep(args.period)
 
 def set_logging():
     logger = logging.getLogger()
@@ -39,12 +41,22 @@ def set_logging():
     handler.setFormatter(fmt)
     logger.addHandler(handler)
 
-# def parse_cmdline(
+def parse_cmdline():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', dest='size', type=int, default=DEFAULT_POOL_SIZE,
+            help=f"maximum number of concurrent tasks. Default: {DEFAULT_POOL_SIZE}")
+    parser.add_argument('-p', dest='period', type=float, default=DEFAULT_PERIOD,
+            help=f"period between scheduling new jobs. Default: {DEFAULT_PERIOD}s")
+    parser.add_argument('-t', dest='timeout', type=float, default=DEFAULT_TIMEOUT,
+            help=f"timeout for the jobs. Default: {DEFAULT_TIMEOUT}s")
+
+    return parser.parse_args()
 
 if __name__ == '__main__':
     set_logging()
+    args = parse_cmdline()
     try:
-        asyncio.run(main())
+        asyncio.run(main(args))
     except RuntimeError:
         # Likely you pressed Ctrl-C...
         ...
